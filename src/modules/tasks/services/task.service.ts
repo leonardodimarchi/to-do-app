05d3266a3ca2b7b430fbde558ba36a72cf2fcddd
Leonardo Dimarchi <leonardodimarchi@gmail.com>
2021-06-29 10:48:44 -0300

    update(tasks): finishing the base crud methods

--- src/modules/tasks/services/task.service.ts
+++ src/modules/tasks/services/task.service.ts
@@ -1,34 +1,67 @@
-import { Injectable } from "@nestjs/common";
-import { Task } from "../models/task";
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { Task } from '../models/task';
 
 @Injectable()
 export class TaskService {
   tasks: Task[] = [
-    { id: 1, description: "Task 1", completed: false },
-    { id: 2, description: "Task 2", completed: false },
-    { id: 3, description: "Task 3", completed: true },
-    { id: 4, description: "Task 4", completed: false }
+    { id: 1, description: 'Task 1', completed: false },
+    { id: 2, description: 'Task 2', completed: false },
+    { id: 3, description: 'Task 3', completed: true },
+    { id: 4, description: 'Task 4', completed: false },
   ];
 
-  getAll(): Task[] {
+  /**
+   * Retorna todas as tasks
+   */
+  public getAll(): Task[] {
     return this.tasks;
   }
 
-  getById(id: number): Task {
-    const selectedTask = this.tasks.find((task) => task.id === id);
+  /**
+   * Retorna uma task a partir da identificação
+   */
+  public getById(id: number): Task {
+    const selectedTask = this.tasks.find((task) => task.id === id );
+
+    if (!selectedTask)
+      throw new ForbiddenException(`A task com o id: ${id} procurado, não pode ser encontrada`)
 
     return selectedTask;
   }
 
-  create(task: Task) {
+  /**
+   * Cria uma nova entidade de task
+   * @param task As informações da task
+   */
+  public create(task: Task): Task {
+    task.id = this.tasks.length > 0 ? this.tasks[this.tasks.length - 1].id + 1 : 1;
+    this.tasks.push(task);
 
+    return task;
   }
 
-  update(task: Task) {
+  /**
+   * Atualiza uma entidade de task
+   * @param task As informações da task
+   */
+  public update(task: Task): Task {
+    const oldTask = this.getById(+task.id);
 
+    if (!oldTask)
+      throw new ForbiddenException('A task procurada para atualização não pode ser encontrada')
+
+    oldTask.description = task.description;
+    oldTask.completed = task.completed;
+
+    return oldTask;
   }
 
-  delete(id: number) {
+  /**
+   * Deleta uma entidade de task a partir de sua identificação
+   */
+  public delete(id: number): void {
+    const taskIndex = this.tasks.findIndex((task) => task.id === id);
 
+    this.tasks.splice(taskIndex, 1);
   }
 }
--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Get, Param } from "@nestjs/common";
-import { TaskService } from "./services/task.service";
-import { Task } from "./models/task";
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { TaskService } from './services/task.service';
+import { Task } from './models/task';
 
 @Controller('tasks')
 export class TasksController {
@@ -10,11 +10,28 @@ export class TasksController {
 
   @Get()
   public async getAll(): Promise<Task[]> {
-    return this.taskService.getAll();
+    return  this.taskService.getAll();
   }
 
-  @Get('/:id')
-  public async getById(@Param('id') id: number): Promise<Task> {
-    return this.taskService.getById(id);
+  @Get(':id')
+  public async getById(@Param('id') id: string): Promise<Task> {
+    return this.taskService.getById(+id);
+  }
+
+  @Post()
+  public async create(@Body() task: Task): Promise<Task> {
+    return this.taskService.create(task);
+  }
+
+  @Put(':id')
+  public async update(@Param('id') id: string, @Body() task: Task): Promise<Task> {
+    task.id = +id;
+
+    return this.taskService.update(task);
+  }
+
+  @Delete(':id')
+  public async delete(@Param('id') id: string): Promise<void> {
+    return this.taskService.delete(+id);
   }
 }
