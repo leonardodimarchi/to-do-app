32683a757fab1bd0e36531c98568d9b15fdf4a20
Leonardo Dimarchi <leonardodimarchi@gmail.com>
2021-07-01 10:19:02 -0300

    update(task): adding typeorm module at tasks module

--- src/modules/tasks/tasks.module.ts
+++ src/modules/tasks/tasks.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskEntity } from './entities/task.entity';
 import { TaskService } from "./services/task.service";
 import { TasksController } from "./tasks.controller";
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      TaskEntity
+    ])
+  ],
   exports: [
     TaskService,
   ],
