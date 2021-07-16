a6aa302e9cbe5340efcd4b276f68783440d95ad9
leonardo <leonardodimarchi@outlook.com>
2021-07-16 11:36:15 -0300

    update(ormconfig): disabling eslint

--- ormconfig.js
+++ ormconfig.js
@@ -1,4 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const envalid = require('envalid');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { join } = require('path');
 
 const rule = {
