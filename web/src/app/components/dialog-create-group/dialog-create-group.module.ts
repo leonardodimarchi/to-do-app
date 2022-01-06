4ffdfafc912057b79110fbf8c26831b65a58d29f
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2022-01-05 22:16:18 -0300

    update(create-group-dialog): adding close button

--- web/src/app/components/dialog-create-group/dialog-create-group.module.ts
+++ web/src/app/components/dialog-create-group/dialog-create-group.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { DialogCreateGroupComponent } from './dialog-create-group.component';
 
@@ -15,6 +16,7 @@ import { DialogCreateGroupComponent } from './dialog-create-group.component';
     FormsModule,
     MatInputModule,
     MatButtonModule,
+    MatIconModule,
   ],
   declarations: [
     DialogCreateGroupComponent
